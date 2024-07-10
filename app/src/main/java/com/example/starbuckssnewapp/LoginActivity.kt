53e965b0bb14e027ff9e6df6ccfbe869428a71d8
anabeatrizjor <biajordaoh@gmail.com>
2024-07-10 16:08:40 -0300

    Novas telas adicionadas
    tela de login editada em questões de layout;tela de cadastro editada em questões de layout;tela 'home' iniciada; t'welcome screen' adicionada (não modificada)

--- app/src/main/java/com/example/starbuckssnewapp/LoginActivity.kt
+++ app/src/main/java/com/example/starbuckssnewapp/LoginActivity.kt
@@ -1,16 +1,40 @@
 package com.example.starbuckssnewapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val emailInput = findViewById<EditText>(R.id.emailInputLogin)
+        val passwordInput = findViewById<EditText>(R.id.passwordInputLogin)
+        val newAccBtn = findViewById<Button>(R.id.ButtonCadastro)
+        val loginBtn = findViewById<EditText>(R.id.ButtonCadastro)
+        val backArrow = findViewById<ImageView>(R.id.backArrowLogin)
+
+        newAccBtn.setOnClickListener {
+            val intent = Intent(this@LoginActivity, CadastroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        backArrow.setOnClickListener {
+            val intent = Intent(this@LoginActivity, CadastroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
 
     }
