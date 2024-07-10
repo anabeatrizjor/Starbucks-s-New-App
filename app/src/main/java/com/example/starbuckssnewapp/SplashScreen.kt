23f8c0679d903d48a12d7c4ebb625dddbf926af8
anabeatrizjor <biajordaoh@gmail.com>
2024-07-10 15:07:50 -0300

    Splash Screen
    tela de splashscreen criada; inicialização da tela de login; inicialização da tela de cadastro; inicialização da tela do menu

---
+++ app/src/main/java/com/example/starbuckssnewapp/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.starbuckssnewapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+
+    private val splashScreenStarted = 2000
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.splash_screen)
+
+        val logoView = findViewById<ImageView>(R.id.logo)
+        val animView = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashScreenStarted.toLong())
+    }
+}
